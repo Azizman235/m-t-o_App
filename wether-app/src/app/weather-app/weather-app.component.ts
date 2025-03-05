556da54ab1d24e6592beccd967cf4a5687466e0b
Azizman235 <mahamatzene.abdel-aziz@etud.univ-angers.fr>
2025-03-05 23:14:19 +0000

    nouveau projet angular

---
+++ wether-app/src/app/weather-app/weather-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-app',
+  imports: [],
+  templateUrl: './weather-app.component.html',
+  styleUrl: './weather-app.component.scss'
+})
+export class WeatherAppComponent {
+
+}
